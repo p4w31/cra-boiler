3c322b9731bd712807305f7d1b2cc13192ec06ba
Pawel <szalewskipawel@gmail.com>
2019-01-04 17:44:33 +0100

    prepareComponent

--- src/pages/App/testChanger.test.js
+++ src/pages/App/testChanger.test.js
@@ -4,15 +4,21 @@ import { mount, shallow, render } from 'enzyme';
 import { TestChanger } from './testChanger';
 import { findByTestAttr, checkProps } from '../../test/helpers';
 
+let wrapped = null;
+
 const defaultProps = {
     changeTestState: function() {}
 };
 
+const prepareComponent = (props={}) => {
+    const myProps = { ...defaultProps, ...props };
+    wrapped = shallow(<TestChanger {...myProps} />);
+    return wrapped;
+}
+
 describe('<TestChanger />', () => {
     it('renders without crashing', () => {
-        let wrapped = null;
-        wrapped = shallow(<TestChanger />);
-        //expect(wrapped.find('.test-changer-wrapper').length).toBe(1);
+        wrapped = prepareComponent();
         expect( findByTestAttr(wrapped, 'test-changer-component') ).toHaveLength(1);
     });
 
